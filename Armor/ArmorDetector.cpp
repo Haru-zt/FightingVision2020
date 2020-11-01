8624314befcb6527c26e3b8c6e9d79977a63c0ad
ZXiiiC <72813202+ZXiiiC@users.noreply.github.com>
2020-11-01 18:48:28 +0800

    Update ArmorDetector.cpp
    ImgTransfer_Bin函数 通道相减法处理照片，获得灯条的二值图 需要设定灯条颜色

--- Armor/ArmorDetector.cpp
+++ Armor/ArmorDetector.cpp
@@ -9,6 +9,34 @@ ArmorDetector::~ArmorDetector()
 {
 }
 
+void ArmorDetector::ImgTransfm_Bin()
+{
+    int MatSize=Img_ori.rows*Img_ori.cols;
+    Img_bin=Mat::zeros(Img_ori.size(),CV_8UC1);
+    uchar* ori_data=(uchar *)Img_ori.data;
+    uchar* bin_data=(uchar *)Img_bin.data;
+    if(enermyColor==Red)
+    {
+    for(int i=0;i<MatSize;i++)
+    {
+        if(*(ori_data+2)-*ori_data>Color_Threshold)
+            *bin_data=255;
+        ori_data+=3;
+        bin_data++;
+    }
+    }
+    else if(enermyColor==Blue)
+    {
+        for(int i=0;i<MatSize;i++)
+    {
+        if(*ori_data-*(ori_data+2)>Color_Threshold)
+            *bin_data=255;
+        ori_data+=3;
+        bin_data++;
+    }
+    }
+}
+
 bool ArmorDetector::DetectArmor(cv::Mat& src, cv::Point3f& target)
 {
 	cv::imshow("src", src);
